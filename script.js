bf1b5ecce781bb6926e93d611c4b5bde4a573085
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-22 16:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function shortcut(s1, s2) {
-  // your code here
+  s1 = s1.trim()
+s2 = s2.trim()
+
+let str = ""
+if(s1 == "" || s2 == ""){
+  let ans = ""
+  return ans
+}
+else{
+  str += s1.charAt(0)
+  str += s2.charAt(0)
+}
+return str
 }
 
 // Do not change the code below.
